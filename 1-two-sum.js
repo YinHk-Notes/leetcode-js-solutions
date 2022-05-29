de44d5a023dc1e2bb7d7262ee0a9f4b4ee3c432f
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-28 22:05:21 -0400

    Update 1-two-sum.js

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 const twoSum = function(nums, target) {
-  const myObject = {};
+  const myObject = {};             //create an object to store the complement value and its index 
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
     if (myObject.hasOwnProperty(complement)) {
       return [myObject[complement], i];
     }
-    myObject[nums[i]] = i;
+    myObject[nums[i]] = i;         //return the object with one of the value and index pair
   }
 };
